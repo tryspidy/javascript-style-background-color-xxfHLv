b64168cec07f21e471bb0333262191bd7e9e0792
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 16:59:35 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+ // change background color for specific id ..
+function changebackground(){
+	document.getElementById('id').style.backgroundColor = 'green' ; 
+}
+// change background color for whole body..
+function changebackground(){
+	document.body.style.backgroundColor = 'green';
+}
+ 
